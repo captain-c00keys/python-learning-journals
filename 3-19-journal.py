079bed8952d6b3bd1a6a33b20a4b13902062b384
captain-c00keys <gpieterson.wsu@hotmail.com>
2018-03-19 19:24:27 -0700

    adding first journal

---
+++ 3-19-journal.py
@@ -0,0 +1,3 @@
+# Journal 3/19
+
+Today, we definitely learned a lot. Brandy did a wonderful job explaining how to brand yourself. Then we learned a lot of code that we've seen in our prework, but this time, it would help us to build the snakes cafe app. It was challenging but good. There were definitely moments where we had two different options to perform the tasks at hand. Now it's time to learn from the readings and videos and to finish my prework.
